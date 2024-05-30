621156a171cc4ace904ab11d655b0e917e72d8e9
Dracowyn <dracowyn@outlook.com>
2024-05-30 15:31:04 +0800

    自定义用户语言包

---
+++ app/api/lang/zh-cn/card/user.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author Dracowyn
+ * @since 2024/5/29 下午5:41
+ */
+
+return [
+	'Please login first' => '请先登录！',
+	'You have no permission' => '没有权限操作！',
+	'username' => '用户名',
+	'email' => '邮箱',
+	'mobile' => '手机号',
+	'password' => '密码',
+	'keep' => '保持登录状态',
+	'Please input correct username' => '请输入正确的用户名',
+	'Username already exists' => '用户名已存在',
+	'Please input username' => '请输入用户名',
+	'Email already exists' => '邮箱已存在',
+	'Please input email' => '请输入邮箱',
+	'Please input correct email' => '请输入正确的邮箱',
+	'Please input correct mobile' => '请输入正确的手机号',
+	'Mobile already exists' => '手机号已存在',
+	'Please input mobile' => '请输入手机号',
+	'Please input correct password' => '请输入正确的密码',
+	'Please input correct keep' => '请输入正确的保持登录状态',
+	'Login successful' => '登录成功',
+	'You have already logged in. There is no need to log in again~' => '您已经登录了，无需再次登录~',
+	'Registration successful' => '注册成功',
+	'Get success' => '获取成功',
+];
