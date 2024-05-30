d6f2025ab17a8f17afa553f282c7de128d135d4c
Dracowyn <dracowyn@outlook.com>
2024-05-30 20:45:11 +0800

    新增添加通讯录名片控制器

--- app/api/controller/card/Card.php
+++ app/api/controller/card/Card.php
@@ -83,4 +83,65 @@ class Card extends Frontend
 			$this->error(__('Method not allowed'));
 		}
 	}
+
+	/**
+	 * 添加名片
+	 * @return void
+	 * @return void
+	 * @throws Throwable
+	 * @throws DataNotFoundException
+	 * @throws DbException
+	 * @throws ModelNotFoundException
+	 */
+	public function add(): void
+	{
+		if ($this->request->isPost()) {
+			$params    = $this->request->post(['nickname', 'mobile', 'gender', 'city', 'remark', 'type']);
+			$cardModel = new \app\common\model\card\Card();
+			$typeModel = new Type();
+			$validate  = new \app\api\validate\card\Card();
+			$userInfo  = $this->auth->getUserInfo();
+
+			try {
+				$validate->scene('add')->check($params);
+			} catch (Throwable $e) {
+				$this->error($e->getMessage());
+			}
+
+			// 验证type_id和user_id是否在type表中存在
+			$where = [
+				['id', '=', $params['type']],
+				['user_id', '=', $userInfo['id']]
+			];
+			$type  = $typeModel->where($where)->find();
+			if (!$type) {
+				$this->error(__('Type not exists'));
+			}
+
+			// 查找是否存在相同手机号
+			$map  = [
+				['mobile', '=', $params['mobile']],
+				['user_id', '=', $userInfo['id']]
+			];
+			$card = $cardModel->where($map)->find();
+
+			if ($card) {
+				$this->error(__('Mobile already exists'));
+			}
+
+			$params['user_id'] = $userInfo['id'];
+			$params['type_id'] = $type['id'];
+			unset($params['type']);
+
+			$res = $cardModel->save($params);
+
+			if ($res) {
+				$this->success(__('Add success'));
+			} else {
+				$this->error(__('Add failed'));
+			}
+		} else {
+			$this->error(__('Method not allowed'));
+		}
+	}
 }
