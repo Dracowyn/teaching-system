eb07132f63dc5b77062b70d23ae564135db4b4d1
Dracowyn <dracowyn@outlook.com>
2024-05-30 20:43:02 +0800

    加入分类名称字段

--- app/common/model/card/Card.php
+++ app/common/model/card/Card.php
@@ -3,52 +3,61 @@
 namespace app\common\model\card;
 
 use think\Model;
+use think\model\relation\BelongsTo;
 
 /**
  * Card
  */
 class Card extends Model
 {
-    // 表名
-    protected $name = 'card';
+	// 表名
+	protected $name = 'card';
 
-    // 自动写入时间戳字段
-    protected $autoWriteTimestamp = true;
+	// 自动写入时间戳字段
+	protected $autoWriteTimestamp = true;
 
-    // 追加属性
-    protected $append = [
-        'city_text',
-    ];
+	// 追加属性
+	protected $append = [
+		'city_text',
+		'type_text',
+	];
 
 
-    public function getCityAttr($value): array
-    {
-        if ($value === '' || $value === null) return [];
-        if (!is_array($value)) {
-            return explode(',', $value);
-        }
-        return $value;
-    }
+	public function getCityAttr($value): array
+	{
+		if ($value === '' || $value === null) return [];
+		if (!is_array($value)) {
+			return explode(',', $value);
+		}
+		return $value;
+	}
 
-    public function setCityAttr($value): string
-    {
-        return is_array($value) ? implode(',', $value) : $value;
-    }
+	public function setCityAttr($value): string
+	{
+		return is_array($value) ? implode(',', $value) : $value;
+	}
 
-    public function getCityTextAttr($value, $row): string
-    {
-        if ($row['city'] === '' || $row['city'] === null) return '';
-        $cityNames = \think\facade\Db::name('area')->whereIn('id', $row['city'])->column('name');
-        return $cityNames ? implode(',', $cityNames) : '';
-    }
+	public function getCityTextAttr($value, $row): string
+	{
+		if ($row['city'] === '' || $row['city'] === null) return '';
+		$cityNames = \think\facade\Db::name('area')->whereIn('id', $row['city'])->column('name');
+		return $cityNames ? implode(',', $cityNames) : '';
+	}
 
-    public function type(): \think\model\relation\BelongsTo
-    {
-        return $this->belongsTo(\app\common\model\card\Type::class, 'type_id', 'id');
-    }
+	public function type(): BelongsTo
+	{
+		return $this->belongsTo(Type::class, 'type_id', 'id');
+	}
 
-    public function user(): \think\model\relation\BelongsTo
-    {
-        return $this->belongsTo(\app\admin\model\User::class, 'user_id', 'id');
-    }
+	public function user(): BelongsTo
+	{
+		return $this->belongsTo(\app\admin\model\User::class, 'user_id', 'id');
+	}
+
+	public function getTypeTextAttr($value, $row): string
+	{
+		// 根据当前type_id获取type表中的name
+		$type = Type::where('id', $row['type_id'])->field('name')->find();
+		return $type ? $type['name'] : '';
+	}
 }
