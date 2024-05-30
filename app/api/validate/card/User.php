c1921e0d4dfda2fb2107e33230ad64df7e9daec0
Dracowyn <dracowyn@outlook.com>
2024-05-30 20:43:39 +0800

    格式化

--- app/api/validate/card/User.php
+++ app/api/validate/card/User.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author Dracowyn
- * @since 2024/5/29 下午1:57
+ * @since  2024/5/29 下午1:57
  */
 
 namespace app\api\validate\card;
@@ -14,41 +14,41 @@ class User extends Validate
 
 	protected $rule = [
 		'username' => 'unique:user|regex:/^[\x{4E00}-\x{9FA5}A-Za-z][\x{4E00}-\x{9FA5}A-Za-z0-9_]+$/u|require',
-		'email' => 'email|unique:user',
-		'mobile' => 'mobile|unique:user|require',
+		'email'    => 'email|unique:user',
+		'mobile'   => 'mobile|unique:user|require',
 		'password' => 'require|regex:^(?!.*[&<>"\'\n\r]).{6,32}$',
-		'keep' => 'boolean',
+		'keep'     => 'boolean',
 	];
 
 	/**
 	 * 验证场景
 	 */
 	protected $scene = [
-		'login' => ['password', 'keep'],
+		'login'    => ['password', 'keep'],
 		'register' => ['username', 'email', 'mobile', 'password'],
 	];
 
 	public function __construct()
 	{
-		$this->field = [
+		$this->field   = [
 			'username' => __('username'),
-			'email' => __('email'),
-			'mobile' => __('mobile'),
+			'email'    => __('email'),
+			'mobile'   => __('mobile'),
 			'password' => __('password'),
-			'keep' => __('keep'),
+			'keep'     => __('keep'),
 		];
 		$this->message = array_merge($this->message, [
-			'username.regex' => __('Please input correct username'),
-			'username.unique' => __('Username already exists'),
+			'username.regex'   => __('Please input correct username'),
+			'username.unique'  => __('Username already exists'),
 			'username.require' => __('Please input username'),
-			'email.unique' => __('Email already exists'),
-			'email.require' => __('Please input email'),
-			'email.regex' => __('Please input correct email'),
-			'mobile.regex' => __('Please input correct mobile'),
-			'mobile.unique' => __('Mobile already exists'),
-			'mobile.require' => __('Please input mobile'),
-			'password.regex' => __('Please input correct password'),
-			'keep.boolean' => __('Please input correct keep'),
+			'email.unique'     => __('Email already exists'),
+			'email.require'    => __('Please input email'),
+			'email.regex'      => __('Please input correct email'),
+			'mobile.regex'     => __('Please input correct mobile'),
+			'mobile.unique'    => __('Mobile already exists'),
+			'mobile.require'   => __('Please input mobile'),
+			'password.regex'   => __('Please input correct password'),
+			'keep.boolean'     => __('Please input correct keep'),
 		]);
 		parent::__construct();
 	}
