596b4570559abffb95af7ce72c652fd868cd0c5e
Dracowyn <dracowyn@outlook.com>
2024-05-30 16:21:27 +0800

    名片语言包

---
+++ app/api/lang/zh-cn/card/card.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Dracowyn
+ * @since  2024/5/30 下午4:19
+ */
+
+return [
+	'Please login first' => '请先登录！',
+	'You have no permission' => '没有权限操作！',
+	'search' => '搜索',
+	'type' => '分类',
+	'page' => '页码',
+	'limit' => '每页数量',
+	'Please input correct search' => '请不要乱输入搜索内容',
+	'Please input correct type' => '请输入正确的数字分类ID',
+	'Please input correct page' => '请输入正确的数字页码',
+	'Please input correct limit' => '请输入正确的数字每页数量',
+	'Please input page' => '请输入页码',
+	'Please input limit' => '请输入每页数量',
+	'Get success' => '获取成功',
+];
