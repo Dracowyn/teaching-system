a8ca87b88197a5c4f7ca541781751dbe7d51c905
Dracowyn <dracowyn@outlook.com>
2024-06-01 17:00:35 +0800

    新增通讯录分类语言包

---
+++ app/api/lang/zh-cn/card/type.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author Dracowyn
+ * @since  2024/6/1 下午4:56
+ */
+
+return [
+	// 验证器
+	'search'                         => '搜索',
+	'type'                           => '分类',
+	'page'                           => '页码',
+	'limit'                          => '每页数量',
+	'name'                           => '分类名称',
+	'Please input correct search'    => '请不要乱输入搜索内容',
+	'Please input correct type id'   => '请输入正确的整数型分类ID',
+	'Please input correct page'      => '请输入正确的整数型页码',
+	'Please input correct limit'     => '请输入正确的整数型每页数量',
+	'Please input page'              => '请输入页码',
+	'Please input limit'             => '请输入每页数量',
+	'Please input type name'         => '请输入分类名称',
+	'Please input correct type name' => '请输入正确的分类名称',
+
+	// 控制器
+	'Please login first'             => '请先登录！',
+	'You have no permission'         => '没有权限操作！',
+	'Get success'                    => '获取成功',
+	'Add success'                    => '添加成功',
+	'Edit success'                   => '编辑成功',
+	'Delete success'                 => '删除成功',
+	'Get failed'                     => '获取失败',
+	'Add failed'                     => '添加失败',
+	'Edit failed'                    => '编辑失败',
+	'Delete failed'                  => '删除失败',
+	'Method not allowed'             => '不允许的方法',
+	'Type already exists'            => '分类已存在',
+	'Type not exists'                => '分类不存在',
+];
