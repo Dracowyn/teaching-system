99f8587b51716abe070330435e85edbb6f3c9c4c
Dracowyn <dracowyn@outlook.com>
2024-06-01 20:42:30 +0800

    修改提示信息

--- web/src/lang/common/en/validate.ts
+++ web/src/lang/common/en/validate.ts
@@ -4,7 +4,7 @@ export default {
     'Verification is successful!': 'Verification is successful!',
     'Please click': 'Please click',
     'Please enter the correct mobile number': 'Please enter the correct mobile number',
-    'Please enter the correct account': 'The account requires 3 to 15 characters and contains a-z A-Z 0-9 _',
+    'Please enter the correct account': 'The account requires 3 to 15 characters and must start with a letter and only contain Chinese, letters, numbers, and underscores',
     'Please enter the correct password': 'The password requires 6 to 32 characters and cannot contains & < > " \'',
     'Please enter the correct name': 'Please enter the correct name',
     'Content cannot be empty': 'The content cannot be blank',
