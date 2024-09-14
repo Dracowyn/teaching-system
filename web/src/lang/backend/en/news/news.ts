48bc0c2fc540d0cbbbff85f973b3cf6c0c8d2ca4
Dracowyn <dracowyn@outlook.com>
2024-09-14 12:18:53 +0800

    fix新闻

---
+++ web/src/lang/backend/en/news/news.ts
@@ -0,0 +1,14 @@
+export default {
+    id: 'id',
+    title: 'title',
+    anthor: 'anthor',
+    cover: 'cover',
+    content: 'content',
+    hits: 'hits',
+    category: 'category',
+    'category opt0': 'category opt0',
+    'category opt1': 'category opt1',
+    create_time: 'create_time',
+    update_time: 'update_time',
+    'quick Search Fields': 'id,title,anthor,content',
+}
