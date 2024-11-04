96ea2baaf72e59a23e786498dcd610235890a732
Dracowyn <dracowyn@outlook.com>
2024-11-04 10:08:22 +0800

    fix:丢失的语言包文件

---
+++ web/src/lang/backend/en/app/name.ts
@@ -0,0 +1,14 @@
+export default {
+    id: 'id',
+    name: 'name',
+    pkg_name: 'pkg_name',
+    type: 'type',
+    'type mobile': 'type mobile',
+    'type desktop': 'type desktop',
+    'type web': 'type web',
+    icon: 'icon',
+    weigh: 'weigh',
+    create_time: 'create_time',
+    update_time: 'update_time',
+    'quick Search Fields': 'id',
+}
---
+++ web/src/lang/backend/zh-cn/app/name.ts
@@ -0,0 +1,14 @@
+export default {
+    id: 'ID',
+    name: '应用名称',
+    pkg_name: '应用包名',
+    type: '应用类型',
+    'type mobile': '移动端应用',
+    'type desktop': '桌面端应用',
+    'type web': 'Webapp',
+    icon: '应用图标',
+    weigh: '权重',
+    create_time: '创建时间',
+    update_time: '修改时间',
+    'quick Search Fields': 'ID',
+}
