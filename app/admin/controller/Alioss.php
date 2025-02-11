919fcdc71e8494ba6437065c1172a4d5b28d632e
Dracowyn <dracowyn@outlook.com>
2025-02-11 20:51:55 +0800

    feat:阿里云上传接口加入topic参数支持

--- app/admin/controller/Alioss.php
+++ app/admin/controller/Alioss.php
@@ -7,37 +7,50 @@ use app\common\controller\Backend;
 
 class Alioss extends Backend
 {
-    protected array $noNeedPermission = ['callback'];
+	protected array $noNeedPermission = ['callback'];
 
-    /**
-     * 细目
-     * @var string
-     */
-    protected string $topic = 'default';
+	/**
+	 * 细目
+	 * @var string
+	 */
+	protected string $topic = 'default';
 
-    public function initialize(): void
-    {
-        parent::initialize();
-    }
+	public function initialize(): void
+	{
+		parent::initialize();
+	}
 
-    public function callback(): void
-    {
-        $data       = $this->request->post();
-        $params     = [
-            'topic'    => $this->topic,
-            'admin_id' => $this->auth->id,
-            'user_id'  => 0,
-            'url'      => $data['url'],
-            'width'    => $data['width'] ?? 0,
-            'height'   => $data['height'] ?? 0,
-            'name'     => mb_substr(htmlspecialchars(strip_tags($data['name'])), 0, 100),
-            'size'     => $data['size'],
-            'mimetype' => $data['type'],
-            'storage'  => 'alioss',
-            'sha1'     => $data['sha1']
-        ];
-        $attachment = new Attachment();
-        $attachment->data(array_filter($params));
-        $attachment->save();
-    }
+	/**
+	 * 设置细目（存储目录）
+	 */
+	public function setTopic(string $topic): void
+	{
+		$this->topic = $topic;
+	}
+
+	public function callback(): void
+	{
+		$data = $this->request->post();
+
+		if ($data['topic']) {
+			$this->setTopic($data['topic']);
+		}
+
+		$params     = [
+			'topic'    => $this->topic,
+			'admin_id' => $this->auth->id,
+			'user_id'  => 0,
+			'url'      => $data['url'],
+			'width'    => $data['width'] ?? 0,
+			'height'   => $data['height'] ?? 0,
+			'name'     => mb_substr(htmlspecialchars(strip_tags($data['name'])), 0, 100),
+			'size'     => $data['size'],
+			'mimetype' => $data['type'],
+			'storage'  => 'alioss',
+			'sha1'     => $data['sha1']
+		];
+		$attachment = new Attachment();
+		$attachment->data(array_filter($params));
+		$attachment->save();
+	}
 }
