405f863b4974c14a9e3afa95a3edd3c3a8b200ee
Dracowyn <dracowyn@outlook.com>
2025-02-11 18:11:43 +0800

    feat:初步实现新闻列表接口

---
+++ app/api/controller/News.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace app\api\controller;
+
+use app\common\controller\Frontend;
+use think\db\exception\DataNotFoundException;
+use think\db\exception\DbException;
+use think\db\exception\ModelNotFoundException;
+use Throwable;
+
+class News extends Frontend
+{
+	protected array $noNeedLogin      = ['*'];
+	protected array $noNeedPermission = ['*'];
+
+	public function initialize(): void
+	{
+		parent::initialize();
+	}
+
+	/**
+	 * 获取新闻列表
+	 * @return void
+	 * @throws Throwable
+	 */
+	public function index(): void
+	{
+		$param        = $this->request->param(['cid', 'limit', 'page']);
+		$newsModel    = new \app\common\model\news\News();
+		$validate     = new \app\api\validate\News();
+		$uploadConfig = get_upload_config();
+
+		$uploadCdnUrl = '';
+		if ($uploadConfig['mode'] !== 'local') {
+			$uploadCdnUrl = get_sys_config('upload_cdn_url');
+		}
+
+		// 验证参数
+		$validate->scene('index')->check($param);
+
+		// 获取新闻列表
+		$list = $newsModel
+			->field(['id', 'title', 'author', 'cover', 'category', 'create_time'])
+			->whereFindInSet('category', $param['cid'])
+			->order('id', 'desc')
+			->page($param['page'], $param['limit'])
+			->select();
+
+		// 重新组装数据
+		$listData = [];
+		foreach ($list as $key => $value) {
+			$listData[$key]['id']        = $value['id'];
+			$listData[$key]['title']     = $value['title'];
+			$listData[$key]['author']    = $value['author'];
+			$listData[$key]['cover']     = $uploadCdnUrl . $value['cover'];
+			$listData[$key]['category']  = $param['cid'];
+			$listData[$key]['post_time'] = $value['create_time'];
+		}
+
+		$this->success(
+			__('Get success'),
+			[
+				'list'  => $listData,
+				'total' => $newsModel->count(),
+				'page'  => $param['page'],
+				'limit' => $param['limit']
+			]
+		);
+	}
+
+	/**
+	 * 获取新闻详情
+	 * @return void
+	 * @throws DataNotFoundException
+	 * @throws DbException
+	 * @throws ModelNotFoundException
+	 */
+	public function info(): void
+	{
+		$param     = $this->request->param(['id']);
+		$newsModel = new \app\common\model\news\News();
+		$info      = $newsModel
+			->field(['id', 'title', 'author', 'content'])
+			->where('id', $param['id'])
+			->find();
+		$this->success(
+			__('Get success'),
+			[
+				'info' => $info
+			]
+		);
+	}
+}
